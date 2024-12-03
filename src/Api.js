32ef5e13e0a04c15b6c01a6118c5995ed1ed9dce
Cristian <cencarnacioninf13@gmail.com>
2024-12-03 17:49:14 -0400

    Arreglando cosas

--- src/Api.js
+++ src/Api.js
@@ -1,2 +1,2 @@
-export const apiKey = "877faf5b26015cff10f8724df4a3a62a";
-export const url = `https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=${apiKey}&units=metric&lang=es`;
+export const apiKey = "your-api-key";
+export const url = "your-url";
